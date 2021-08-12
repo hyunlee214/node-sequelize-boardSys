3239c63db7fcf89b207704b2e46cb2708c89127b
hyunlee214 <hyun110214@naver.com>
2021-08-12 21:55:34 +0900

    migrate후 db반영완,테이블생성확인

---
+++ models/post.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const { sequelize } = require(".");
+
+// const {
+//   Model
+// } = require('sequelize');
+// module.exports = (sequelize, DataTypes) => {
+//   class post extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+//       // define association here
+//     }
+//   };
+//   post.init({
+//     title: DataTypes.STRING,
+//     writer: DataTypes.STRING
+//   }, {
+//     sequelize,
+//     modelName: 'post',
+//   });
+//   return post;
+// };
+
+module.exports = (sequelize, DataTypes) => {
+  const post = sequelize.define('post', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    writer: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  });
+  return post;
+}
