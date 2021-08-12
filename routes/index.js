390133b114e58b52f42e4d322d4a64e0040873d1
hyunlee214 <hyun110214@naver.com>
2021-08-12 22:31:15 +0900

    브라우저에서 데이터추가성공

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const models = require('../models');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,4 +11,22 @@ router.get('/board', function(req, res, next) {
   res.render('show');
 });
 
+router.post('/board', function(req, res, next) {
+  let body = req.body;
+
+  models.post.create({
+    title: body.inputTitle,
+    writer: body.inputWriter
+  }) 
+    .then(result => {
+      console.log ('data input ok');
+      res.redirect('./board');
+    })
+    .catch(err => {
+      console.log('data input failed,,sorry,,');
+    })
+});
+
 module.exports = router;
+
+
