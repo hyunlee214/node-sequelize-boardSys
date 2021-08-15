2200170e0be7ea9fa657673d23685655430129f8
hyunlee214 <hyun110214@naver.com>
2021-08-15 10:31:26 +0900

    라우터>index수정

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
-// const express = require('express');
-// const models = require('../models');
-// const router = express.Router();
+const express = require('express');
+const models = require('../models');
+const router = express.Router();
 
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'boardSystem' });
-// });
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'boardSystem' });
+});
 
-// // router.get('/board', function(req, res, next) {
-// //   res.render('show');
-// // });
+router.get('/board', function(req, res, next) {
+  res.render('show');
+});
 
 // router.get('/board', function(res, req, next) {
 //   models.post.findAll().then( result => {
@@ -19,182 +19,91 @@
 //   });
 // });
 
-// router.get('/edit/:id', function(res, req, next) {
-//   let postID = req.params.id;
-
-//   models.post.findOne({
-//     where: {id: postID}
-//   })
-//   .then(result => {
-//     res.render('edit', {
-//       post: result
-//     });
-//   })
-//   .catch(err => {
-//     console.log('data read failed,,');
-//   });
-// });
-
-// router.put('/board/:id', function(req, res, next) {
-//   let postId = req.params.id;
-//   let body = req.body;
-
-//   models.post.update({
-//     title: body.editTitle,
-//     writer: body.editWriter
-//   }, {
-//     where: {id: postID}
-//   })
-//   .then(result => {
-//     console.log('data edit ok');
-//     res.redirect('/board');
-//   })
-//   .catch(err => {
-//     console.log('data edit failed...sorry..');
-//   });
-// });
-
-// router.post('/board', function(req, res, next) {
-//   let body = req.body;
-
-//   models.post.create({
-//     title: body.inputTitle,
-//     writer: body.inputWriter
-//   }) 
-//     .then(result => {
-//       console.log ('data input ok');
-//       res.redirect('./board');
-//     })
-//     .catch(err => {
-//       console.log('data input failed,,sorry,,');
-//     })
-// });
-
-// router.delete('/board/:id', function(req, res, next) {
-//   let postID = req.params.id;
-
-//   models.post.destory({
-//     where: {id: postID}
-//   })
-//   .then(result => {
-//     res.redirect('/board')
-//   })
-//   .catch(err=> {
-//     console.log('data delete failed...');
-//   });
-// });
-
-
-// module.exports = router;
-
-
-
-const express = require('express');
-const models = require('../models');
-const router = express.Router();
-
-
-
-// 게시글 목록
-router.get('/board', function(req, res, next) {
+router.get('/borad', function(req, res, next) {
   models.post.findAll({
-    where: {writer: "victolee"}
+    where: {writer: 'hyunlee'}
   })
-      .then( result => {
-        res.render("show", {
-          posts: result
-        });
-      })
-      .catch(function(err){
-        console.log(err);
-      });
+  .then(result => {
+    res.render('show', {
+      post: result
+    });
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
 });
 
-// 게시글 등록
+
 router.post('/board', function(req, res, next) {
   let body = req.body;
 
   models.post.create({
     title: body.inputTitle,
     writer: body.inputWriter
-  })
-  .then( result => {
-    console.log("데이터 추가 완료");
-    res.redirect("/show");
-  })
-  .catch( err => {
-    console.log("데이터 추가 실패");
-  })
+  }) 
+    .then(result => {
+      console.log ('data input ok');
+      res.redirect('./board');
+    })
+    .catch(err => {
+      console.log('data input failed,,sorry,,');
+    })
 });
 
-// 게시글 조회
-router.get('/board/:id', function(req, res, next) {
+
+router.get('/board/id', function(req, res, next) {
   let postID = req.params.id;
 
   models.post.findOne({
-    where: {id: postID}
+    where: {id : postID}
   })
-  .then( result => {
-    res.render("edit", {
+  .then(result => {
+    res.render('edit', {
       post: result
     });
   })
-  .catch( err => {
-    console.log("데이터 조회 실패");
+  .catch(err => {
+    console.log('데이터조회에러')
   });
 });
 
-// 게시글 수정
+
 router.put('/board/:id', function(req, res, next) {
-  let  postID = req.params.id;
+  let postID = req.params.id;
   let body = req.body;
 
   models.post.update({
     title: body.editTitle,
     writer: body.editWriter
-  },{
+  }, {
     where: {id: postID}
   })
-  .then( result => {
-    console.log("데이터 수정 완료");
-    res.redirect("/board");
+  .then(result => {
+    console.log('data edit ok');
+    res.redirect('/board');
   })
-  .catch( err => {
-    console.log("데이터 수정 실패");
+  .catch(err => {
+    console.log('data edit failed...sorry..');
   });
 });
 
-// 게시글 삭제
+
 router.delete('/board/:id', function(req, res, next) {
   let postID = req.params.id;
 
-  models.post.destroy({
+  models.post.destory({
     where: {id: postID}
   })
-  .then( result => {
-    res.redirect("/board")
+  .then(result => {
+    res.redirect('/board')
   })
-  .catch( err => {
-    console.log("데이터 삭제 실패");
+  .catch(err=> {
+    console.log('data delete failed...');
   });
 });
 
-// 댓글 등록
-router.post("/reply/:postID", function(req, res, next){
-  let postID = req.params.postID;
-  let body = req.body;
 
-  models.reply.create({
-    postId: postID,
-    writer: body.replyWriter,
-    content: body.replyContent
-  })
-  .then( results => {
-    res.redirect("/board");
-  })
-  .catch( err => {
-    console.log(err);
-  });
-});
 
 module.exports = router;
+
+
