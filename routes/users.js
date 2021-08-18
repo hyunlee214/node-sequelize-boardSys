1c717fa9688d52e658bb31f7231dd4fe5f63053e
hyunlee214 <hyun110214@naver.com>
2021-08-18 23:29:37 +0900

    내장모듈crypto사용-hashPassword생성*비밀번호암호화

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const models = require('../models');
+const crypto = require('crypto');
 
 /* GET users listing. */
 router.get('/sign_up', function(req, res, next) {
   res.render('user/signup');
 });
 
-router.post("/sign_up", function(req,res,next){
+router.post("/sign_up", async function(req,res,next){
   let body = req.body;
 
-  models.user.create({
+  let inputPassword = body.password;
+  let salt = Math.round((new Date().valueOf() * Math.random())) + "";
+  let hashPassword = crypto.createHash('sha512').update(inputPassword + salt).digest('hex');
+
+  let result = models.user.create({
     name: body.userName,
     email: body.userEmail,
-    password: body.password
+    password: hashPassword,
+    salt: salt
   })
   .then( result => {
     res.redirect("/users/sign_up");
@@ -23,4 +29,16 @@ router.post("/sign_up", function(req,res,next){
   })
 })
 
+
+
 module.exports = router;
+
+
+
+
+
+// crypto.randomBytes(64, (err, salt) => {
+//   crypto.pbkdf2('비밀번호', salt.toSring('base64'), 100000, 64, 'sha512', (err, key) => {
+//     console.log(key.toString('base64'));
+//   });
+// });
