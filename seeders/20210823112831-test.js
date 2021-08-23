450a0df69f7adabcc359d1c7cf1c68cc5f86b1a0
hyunlee214 <hyun110214@naver.com>
2021-08-23 20:44:12 +0900

    Seeders실행 > 데이터 추가

--- seeders/20210823112831-test.js
+++ seeders/20210823112831-test.js
@@ -1,16 +1,23 @@
 'use strict';
 
+// up => "seed를 생성할 때 수행"
+// down => "seed를 되돌릴 때 수행"
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    let datas = [];
+
+    for (let i = 0; i< 10; i++) {
+      let obj = {
+        email: "test" + i + "@example.com",
+        name: "testUser" + i,
+        password: "1234",
+        createdAt: new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''),
+        updatedAt: new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
+      }
+      datas.push(obj)
+    }
+    return queryInterface.bulkInsert('users', datas, {});
   },
 
   down: async (queryInterface, Sequelize) => {
