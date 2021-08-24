64655bbfd4e82cfef6576bab3879f12330d2278f
hyunlee214 <hyun110214@naver.com>
2021-08-24 21:34:59 +0900

    multer미들웨어추가+라우팅

---
+++ routes/upload.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const upload = multer({ dest: 'upload/' })
+
+router.get('/show', function(req, res, next) {
+  res.render('board')
+});
+
+router.post('/create', upload.single('imgFile'), function(req, res, next) {
+  let file = req.file
+  
+  let result = {
+    originalName: file.originalname,
+    size: file.size,
+  }
+});
+
+module.exports = router;
