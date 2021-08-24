3668244e64c6172481103f949fefbe831c0a1d62
hyunlee214 <hyun110214@naver.com>
2021-08-24 11:01:49 +0900

    코드수정

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 "use strict";
 
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const methodOverride = require('method-override');
 const models = require('./models/index.js');
+const session = require('express-session');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
 
 models.sequelize.sync().then(() => {
   console.log('db연결성공');
@@ -9,17 +21,6 @@ models.sequelize.sync().then(() => {
   console.log(err);
 })
 
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const methodOverride = require('method-override');
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-
-const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -34,7 +35,7 @@ app.use(methodOverride('_method'));
 
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/user', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
+/*
 const express = require('express');
 const router = express.Router();
 const models = require('../models');
 const crypto = require('crypto');
 
-/* GET users listing. */
 router.get('/sign_up', function(req, res, next) {
   res.render('user/signup');
 });
@@ -18,7 +18,7 @@ router.post("/sign_up", async function(req, res, next){
   let result = models.user.create({
     name: body.userName,
     email: body.userEmail,
-    password: hashPassword,
+    password: body.password,
     salt: salt
   })
   res.redirect('/user/sign_up');
@@ -29,7 +29,11 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('user/login');
+  let session = req.session;
+
+  res.render('user/login', {
+    session : session
+  });
 });
 
 router.post('/login', async function(req, res, next) {
@@ -43,7 +47,7 @@ router.post('/login', async function(req, res, next) {
 
   let dbPassword = result.dataVaules.password;
   let inputPassword = body.password;
-  let salt = result.dataVaules.salt;
+  let salt = result.dataValues.salt;
   let hashPassword = crypto.createHash('sha512').update(inputPassword + salt).digest('hex');
 
   if (dbPassword === hashPassword) {
@@ -72,3 +76,79 @@ module.exports = router;
 //     console.log(key.toString('base64'));
 //   });
 // });
+
+*/
+
+const express = require('express');
+const router = express.Router();
+const crypto = require('crypto');
+const models = require('../models');
+
+router.get('/sign_up', function(req, res, next) {
+  res.render('user/signup');
+});
+
+router.post('/sign_up', async function(req, res, next) {
+  let body = req.body;
+
+  //crypto 모듈 사용 => hashPassword생성
+
+  let inputPassword = body.password;
+  let salt = Math.round((new Date().valueOf() * Math.random())) + "";      
+  // salt적용해서 암호화 강화 (현재 시간 * 랜덤값으로 문자열 생성)
+  let hashPassword = crypto.createHash('sha512').update(inputPassword + salt).digest('hex');
+  // 암호화 강화를 위해 'sha512 알고리즘' 사용
+
+  let result = models.user.create({
+    name: body.userName,
+    email: body.userEmail,   
+    //password: hashPassword,                
+    password:body.password,                  
+    salt: salt                    
+  })
+    res.redirect('/user/sign_up');
+  })
+
+
+  router.get('/', function(req, res, next) {
+    if (req.cookies) {
+      console.log(req.cookies);
+    }
+    res.send('환영합니다');
+  });
+
+  router.get('/login', function(req, res, next) {
+      let session = req.session;
+
+      res.render('user/login', {
+        session : session
+      });
+  });
+
+  router.post('/login', async function(req, res, next) {
+    let body = req.body;
+
+    let result = await models.user.findOne({
+      where: {
+        email: body.userEmail
+      }
+    });
+
+    let dbPassword = result.dataValues.password;
+    let inputPassword = body.password;
+    let salt = result.dataValues.salt;
+    let hashPassword = crypto.createHash("sha512").update(inputPassword + salt).digest("hex");
+
+    if(dbPassword === hashPassword){
+        console.log("비밀번호 일치");
+
+        req.session.email = body.userEmail;
+        res.redirect('/user');
+    }
+    else{
+        console.log("비밀번호 불일치");
+        res.redirect('/user/login');
+  }
+});
+
+module.exports = router;
